1dee093407be147271a59714e5aac4f5b264a826
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-06-21 11:56:15 +0200

    UPDATE: same as before

--- src/providers/DestinationProvider.jsx
+++ src/providers/DestinationProvider.jsx
@@ -6,17 +6,19 @@ import { toursToRenderArrayConstructor } from "../utils/toursToRenderArrayConstr
 export const DestinationContext = createContext();
 
 export const DestinationProvider = ({ children }) => {
-  const { allDestinations, loading, reloadDestinations } = useContext(AllDestinationsContext);
+  const { allDestinations, loading, reloadDestinations } = useContext(
+    AllDestinationsContext
+  );
   const { destination_id } = useParams();
   const [destination, setDestination] = useState(null);
 
   useEffect(() => {
-    if (destination_id) {
-      const actualDestination = allDestinations.find(destination => destination._id===destination_id);
-      if(actualDestination==undefined) {reloadDestinations()}else{
-        const toursToRender = toursToRenderArrayConstructor(actualDestination);
-        setDestination({...actualDestination, tours: toursToRender });
-      }
+    if (destination_id && !loading) {
+      const actualDestination = allDestinations.find(
+        (destination) => destination._id === destination_id
+      );
+      const toursToRender = toursToRenderArrayConstructor(actualDestination);
+      setDestination({ ...actualDestination, tours: toursToRender });
     }
   }, [destination_id, allDestinations]);
 
