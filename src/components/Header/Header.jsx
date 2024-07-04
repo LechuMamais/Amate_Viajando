3f8a29dc7f48ceae6b7730915f54ff45de86b11f
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-07-04 13:18:40 +0200

    UPDATE: Header uses colored logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import { Box, Flex, IconButton, Spacer } from "@chakra-ui/react";
+import { Box, Flex, IconButton, Image, Spacer } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import {
@@ -17,54 +17,63 @@ import { UserContext } from "../../providers/UserProvider";
 const Header = () => {
   const { user } = useContext(UserContext);
   return (
-    <header>
-      <Flex>
-        <Box p="4">
-          <Link to={"/"}>Amate</Link>
-        </Box>
-        <Spacer />
-        <Box p="4">
-          <Menu>
-            <MenuButton
-              as={IconButton}
-              aria-label="Options"
-              icon={<HamburgerIcon />}
-              variant="outline"
-            />
-            <MenuList className="header-menu-list">
-              <MenuGroup title="Viaja">
-                <MyLink to="/destinations">
-                  <MenuItem>Destinos</MenuItem>
-                </MyLink>
-                <MyLink to="/tours">
-                  <MenuItem>Tours</MenuItem>
-                </MyLink>
-                <MenuDivider />
-                <MenuGroup title="Perfil">
-                  {user.logged ? (
-                    <>
-                      <MyLink to={"/profile"}>
-                        <MenuItem>Mi cuenta</MenuItem>
-                      </MyLink>
-                      <MyLink to={"/carrito"}>
-                        <MenuItem>Ver carrito</MenuItem>
-                      </MyLink>
-                      <MyLink to={"/logout"}>
-                        <MenuItem color={'red.400'} pl={4}>Cerrar Sesión</MenuItem>
-                      </MyLink>
-                    </>
-                  ) : (
-                    <MyLink to={"/login"}>
-                      <MenuItem>Login</MenuItem>
+    <Flex as="header">
+      <Box>
+        <Link to={"/"}>
+          <Image
+            src="/assets/logo_header.jpg"
+            alt="Amate Viajando"
+            objectFit="cover"
+            w="72px"
+            h="72px"
+            position="absolute"
+          />
+        </Link>
+      </Box>
+      <Spacer />
+      <Box p="4">
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+            variant="outline"
+          />
+          <MenuList className="header-menu-list">
+            <MenuGroup title="Viaja">
+              <MyLink to="/destinations">
+                <MenuItem>Destinos</MenuItem>
+              </MyLink>
+              <MyLink to="/tours">
+                <MenuItem>Tours</MenuItem>
+              </MyLink>
+              <MenuDivider />
+              <MenuGroup title="Perfil">
+                {user.logged ? (
+                  <>
+                    <MyLink to={"/profile"}>
+                      <MenuItem>Mi cuenta</MenuItem>
                     </MyLink>
-                  )}
-                </MenuGroup>
+                    <MyLink to={"/carrito"}>
+                      <MenuItem>Ver carrito</MenuItem>
+                    </MyLink>
+                    <MyLink to={"/logout"}>
+                      <MenuItem color={"red.400"} pl={4}>
+                        Cerrar Sesión
+                      </MenuItem>
+                    </MyLink>
+                  </>
+                ) : (
+                  <MyLink to={"/login"}>
+                    <MenuItem>Login</MenuItem>
+                  </MyLink>
+                )}
               </MenuGroup>
-            </MenuList>
-          </Menu>
-        </Box>
-      </Flex>
-    </header>
+            </MenuGroup>
+          </MenuList>
+        </Menu>
+      </Box>
+    </Flex>
   );
 };
 
