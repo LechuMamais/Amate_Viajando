c871b4a2ca7ce4b5006238a4fea172809022f1ee
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-07-26 13:12:02 +0200

    Fixed: Overflox X when scrolling on Hero

--- src/components/HeroText/HeroText.jsx
+++ src/components/HeroText/HeroText.jsx
@@ -5,79 +5,85 @@ import { Box, Button, Heading, Text, Flex } from "@chakra-ui/react";
 const HeroText = () => {
   return (
     <Box
-      w="100vw"
-      mx="auto"
+      className="hero-text-wrapper"
+      w="100lvw"
       position="absolute"
       bottom={{ base: "144px", sm: "110px" }}
       zIndex="2"
-      className="hero-text-container"
+      overflow="hidden"
     >
-      <Flex
-        direction="column"
-        textAlign="center"
-        spacing={4}
+      <Box
+        w={{ base: "100%", sm: "90%" }}
         mx="auto"
-        bgColor="rgba(255, 255, 255, 0.25)"
-        px={6}
-        py={4}
-        borderRadius={{ base: "0", md: "16" }}
-        w={{ base: "100vw", md: "700px", lg: "860px"}}
+        className="hero-text-container"
       >
-        <Heading
-          as="h1"
-          size="2xl"
-          fontWeight="bold"
-          lineHeight="tight"
-          color="gray.800"
-          mb={4}
-          fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
-        >
-          Descubre tu propósito viajando
-        </Heading>
-        <Text
-          className="yeseva-one-regular"
-          color="gray.700"
-          fontSize={{
-            base: "0.8rem",
-            sm: "md",
-            md: "lg",
-            lg: "base",
-            xl: "lg",
-          }}
-          width={{
-            base: "300px",
-            sm: "450px",
-            md: "660px",
-            lg: "800x",
-          }}
-          lineHeight="relaxed"
+        <Flex
+          direction="column"
+          textAlign="center"
+          spacing={4}
           mx="auto"
-          mb={6}
-          letterSpacing="1.1px"
+          bgColor="rgba(255, 255, 255, 0.25)"
+          px={6}
+          py={4}
+          borderRadius={{ base: "0", sm: "16" }}
+          w={{ base: "100%", md: "700px", lg: "860px" }}
         >
-          La naturaleza enciende nuestros sentidos, nos transporta a lugares
-          mágicos dónde podremos escuchar nuestra voz interior para iluminar el
-          destino correcto en ésta gran aventura.
-          <br />
-          Están listos?
-        </Text>
-        <MyLink to="/destinations">
-          <Button
-            size="lg"
-            colorScheme="blackAlpha"
-            px={16}
-            _focus={{
-              outline: "none",
-              ring: 1,
-              ringColor: "gray.950",
-              color: "gray.100",
-            }}
-            isDisabled={false}
+          <Heading
+            as="h1"
+            size="2xl"
+            fontWeight="bold"
+            lineHeight="tight"
+            color="gray.800"
+            mb={4}
+            fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
           >
-            Vamos
-          </Button>
-        </MyLink>
-      </Flex>
+            Descubre tu propósito viajando
+          </Heading>
+          <Text
+            className="yeseva-one-regular"
+            color="gray.700"
+            fontSize={{
+              base: "0.8rem",
+              sm: "md",
+              md: "lg",
+              lg: "base",
+              xl: "lg",
+            }}
+            maxW={{
+              base: "300px",
+              sm: "450px",
+              md: "660px",
+              lg: "800x",
+            }}
+            lineHeight="relaxed"
+            mx="auto"
+            mb={6}
+            letterSpacing="1.1px"
+          >
+            La naturaleza enciende nuestros sentidos, nos transporta a lugares
+            mágicos dónde podremos escuchar nuestra voz interior para iluminar
+            el destino correcto en ésta gran aventura.
+            <br />
+            Están listos?
+          </Text>
+          <MyLink to="/destinations">
+            <Button
+              size="lg"
+              colorScheme="blackAlpha"
+              px={16}
+              _focus={{
+                outline: "none",
+                ring: 1,
+                ringColor: "gray.950",
+                color: "gray.100",
+              }}
+              isDisabled={false}
+            >
+              Vamos
+            </Button>
+          </MyLink>
+        </Flex>
+      </Box>
     </Box>
   );
 };
