a0f56c9dd622c2162efdb08a6cccfc185a5d93c3
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-10-03 14:51:48 +0200

    UPDATE: Now ResponsiveCarousel checks movil/pc with touch checking

--- src/components/ResponsiveCarousel/ResponsiveCarousel.jsx
+++ src/components/ResponsiveCarousel/ResponsiveCarousel.jsx
@@ -1,13 +1,22 @@
-import { useBreakpointValue, Box } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Box } from '@chakra-ui/react';
 import MySwiper from '../MySwpiper/MySwiper';
 import MyCarousel from '../MyCarousel/MyCarousel';
 
 const ResponsiveCarousel = ({ obj }) => {
-  const isMobile = useBreakpointValue({ base: true, md: false });
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    const checkTouchDevice = () => {
+      setIsTouchDevice('ontouchstart' in window || navigator.maxTouchPoints > 0);
+    };
+
+    checkTouchDevice();
+  }, []);
 
   return (
     <Box w='100%' h='100%'>
-      {isMobile ? <MySwiper obj={obj} /> : <MyCarousel obj={obj} />}
+      {isTouchDevice ? <MySwiper obj={obj} /> : <MyCarousel obj={obj} />}
     </Box>
   );
 };
