0f0d64221714e66e18488ede73a66e2eaa97dbac
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-11-30 18:50:38 +0100

    FEAT: Browser languaje detection on coachingPage

--- src/pages/Coaching/Coaching.jsx
+++ src/pages/Coaching/Coaching.jsx
@@ -1,19 +1,41 @@
 import { Box, Container, Flex, Heading, Stack, Text } from '@chakra-ui/react';
 import ContactTourButtons from '../../components/ContactTourButtons/ContactTourButtons';
-import { spanishHeading, spanishText } from '../../resources/coachingTexts';
+import { useEffect, useState } from 'react';
+import { CoachingText } from '../../resources/coachingTexts';
 
 const Coaching = () => {
+  const [lang] = useState((navigator.language || navigator.userLanguage).slice(0, 2));
+  const [heading, setHeading] = useState();
+  const [text, setText] = useState();
+
+  useEffect(() => {
+    switch (lang) {
+      case 'es':
+        setHeading(CoachingText.es.heading);
+        setText(CoachingText.es.text);
+        break;
+      case 'it':
+        setHeading(CoachingText.it.heading);
+        setText(CoachingText.it.text);
+        break;
+      default:
+        setHeading(CoachingText.en.heading);
+        setText(CoachingText.en.text);
+        break;
+    }
+  }, [lang]);
+
   return (
     <Container maxW='928px' px={{ base: 4, md: 6 }} py={{ base: 12, md: 24, lg: 32 }}>
       <Flex direction='column' gap={8}>
         <Box px={{ base: 4, sm: 6, md: 0 }}>
           <Heading size='xl' fontWeight='bold' textAlign='center'>
-            {spanishHeading}
+            {heading}
           </Heading>
 
           <Stack spacing={6} my={8}>
-            {Array.isArray(spanishText) &&
-              spanishText.map((paragraph, index) => (
+            {Array.isArray(text) &&
+              text.map((paragraph, index) => (
                 <Text
                   key={index}
                   maxW='90ch'
--- src/resources/coachingTexts.js
+++ src/resources/coachingTexts.js
@@ -13,3 +13,53 @@ export const spanishText = [
     'Viajando el descubrirte será tu pasión, viajando comenzarás a Amarte y valorar lo que verdaderamente te mereces.',
     'AMATE VIAJANDO.',
 ];
+
+export const CoachingText = {
+    es: {
+        heading: '¿Qué es el Coaching Viajero?',
+        text: [
+            'Partimos de la base de que el coaching es un entrenamiento por medio del cuál adquirirás herramientas para poder descubrir tu propósito, tu propósito viajando.',
+            'Aquellos que leen descubrir tu propósito les parecerá quizás un poco esotérico, es verdad, ya que podemos asegurar en una sociedad tan dormida, lo que normalmente se hace es seguir a las masas, seguir al sistema que está impuesto bajo el concepto de normalidad, llevando una vida sin pausa, todo corriendo por encima de las 24 horas diarias que disponemos, sumando al cuerpo un sin números de enfermedades y cohibidos por el stress, envueltos en deudas y atados al romance de la frase un día cambiará.',
+            'Pues detente... realmente esa es la vida que deseas vivir?',
+            'Entre las comodidades de la mentira rodeada en la zona de confort encontramos las ataduras que para el cerebro imposibles de romper de accionar frente a tus pensamientos autodestructivos, que, esta vida es así. Y ahí estás nuevamente en la rutina como un encantamiento de brujas, sin poder escapar porque te quieres convencer que eso está bien. Pasan los años y así en un intenso año de trabajo, woow qué maravillas, tienen tan solo 14 días de vacaciones, bueno si trabajas en la misma silla más de 10 años tendrás la fortuna de un mes. ¿Impresionante no? Luego de trabajar más de 320 días anuales. Y como no podía ser de otra manera y tu vida va tan a prisas ni te ha dado tiempo de disfrutar esas vacaciones. Haz perdido dos días ya entre vuelos y luego ese poco tiempo contado para llamémosle descansar, que bueno, pues llevas tan altos los niveles de cortisol del stress que te tardará un tiempo significado en normalizarlos y cuando eso empezó a suceder ya estás de nuevo en el avión camino a casa, sin ningún cambio significativo y todo vuelve a ser como antes, pero bueno al menos tengo algunas fotos para subir al Instagram. Claro.',
+            'Si todo esto te resuena, pero no quieres ser el de los contados días de vacaciones, es hora que despiertes y emprendas el gran cambio. Tú eres el cambio para ser mejor que tu antigua versión.',
+            'Tienes una esencia única, eres el único responsable de tu vida, nuestro equipo desde Amate Viajando deseamos que reacciones y que descubras lo que hay en tu ADN, eres brillante en una sola cosa y ese es tu potencial.',
+            '¿Estás listo? Da el sí.',
+            'Tu propósito, tu nueva maravillosa vida.',
+            'Tu propósito y el viaje de tus sueños dentro de tu verdadero despertar.',
+            'Viajando en presencia y en conciencia entramos en la tierra fértil hacia nuestra Libertad.',
+            'Viajando el descubrirte será tu pasión, viajando comenzarás a Amarte y valorar lo que verdaderamente te mereces.',
+            'AMATE VIAJANDO.',
+        ]
+    },
+    en: {
+        heading: 'What Travel Coaching is?',
+        text: [
+            'We start from the basis that coaching is a training through which you will acquire tools to discover your purpose, your purpose while traveling.',
+            'Those who read discover your purpose may seem a little esoteric, it is true, since we can assure in such a dormant society, what is normally done is to follow the masses, follow the system that is imposed under the concept of normality, leading a life without pause, everything running beyond the 24 hours a day that we have, adding countless diseases to the body and inhibited by stress, wrapped in debt and tied to the romance of the phrase one day it will change.',
+            'Well stop... is that really the life you want to live?',
+            'Among the comforts of lying surrounded in the comfort zone we find the ties that are impossible for the brain to break to act in the face of your self-destructive thoughts, that, this life is like that. And there you are again in the routine like a witch\'s spell, unable to escape because you want to convince yourself that this is right. The years go by and so in an intense year of work, wow how wonderful, they only have 14 days of vacation, well if you work in the same chair for more than 10 years you will have the fortune of a month. Impressive isn\'t it? After working more than 320 days per year. And how could it be any other way and your life is going so fast you haven\'t even had time to enjoy those vacations. You have already lost two days between flights and then that little time counted to let\'s call it rest, that\'s good, because your cortisol levels from stress are so high that it will take you a significant amount of time to normalize them and when that starts to happen you are already back at it. plane on the way home, without any significant changes and everything goes back to how it was before, but hey at least I have some photos to upload to Instagram. Clear.',
+            'If all this resonates with you, but you don\'t want to be the one with the few days of vacation, it\'s time for you to wake up and make the big change. You are the change to be better than your old version.',
+            'You have a unique essence, you are the only one responsible for your life, our team at Amate Viajando wants you to react and discover what is in your DNA, you are brilliant in only one thing and that is your potential.',
+            'Are you ready? Say yes.',
+            'Your purpose, your new wonderful life.',
+            'Your purpose and the journey of your dreams within your true awakening.',
+            'Traveling in presence and in consciousness we enter the fertile land towards our Freedom.',
+            'Travelling, discovering yourself will be your passion, traveling you will begin to love yourself and value what you truly deserve.',
+            'LOVE YOURSELF TRAVELING.',
+        ]
+    },
+    it: {
+        heading: 'Cos’è il Travel Coaching?',
+        text: [
+            'Partiamo dal presupposto che il coaching è una formazione attraverso la quale acquisirai strumenti per scoprire il tuo scopo, il tuo scopo viaggiando.',
+            'Coloro che leggono Scopri il tuo scopo possono sembrare un po\' esoterici, è vero, poiché possiamo assicurare che in una società così dormiente, ciò che viene fatto normalmente è seguire le masse, seguire il sistema imposto sotto il concetto di normalità, guidando una vita senza pause, tutto che va oltre le 24 ore al giorno che abbiamo, aggiungendo innumerevoli malattie al corpo e inibito dallo stress, avvolto nei debiti e legato al romanticismo della frase un giorno cambierà.',
+            'Bene, smettila... è davvero questa la vita che vuoi vivere?', 'If all this resonates with you, but you don\'t want to be the one with the few days of vacation, it\'s time for you to wake up and make the big change. You are the change to be better than your old version.',
+            'Hai un\'essenza unica, sei l\'unico responsabile della tua vita, il nostro team di Amate Viajando vuole che tu reagisca e scopra cosa c\'è nel tuo DNA, sei brillante solo in una cosa e questo è il tuo potenziale.',
+            'Sei pronto? Di di sì.',
+            'Il tuo scopo, la tua meravigliosa nuova vita.', 'Il tuo scopo e il viaggio dei tuoi sogni verso il tuo vero risveglio.',
+            'Viaggiando nella presenza e nella coscienza entriamo nella terra fertile verso la nostra Libertà.',
+            'Viaggiare, scoprire te stesso sarà la tua passione, viaggiando inizierai ad amare te stesso e ad apprezzare ciò che meriti veramente.',
+            'AMATI VIAGGIANDO.',]
+    },
+};
