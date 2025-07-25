f9c6480e0f4cf807fdbf9e8431a91f8cab918bb6
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2025-07-25 13:41:42 +0200

    UPDATE: Home redefined and redesigned

--- src/components/HeroText/HeroText.jsx
+++ src/components/HeroText/HeroText.jsx
@@ -1,72 +1,52 @@
 import { useTranslation } from 'react-i18next';
 import MyLink from '../../components/MyLink/MyLink';
-import { Box, Button, Heading, Text, Flex } from '@chakra-ui/react';
+import { Box, Button, Text, Flex } from '@chakra-ui/react';
 
 const HeroText = () => {
   const { t } = useTranslation('Home');
 
   return (
     <Box
-      flex={{ base: '0', md: '1' }}
       className='hero-text-wrapper'
       zIndex='2'
       overflow='hidden'
       w={{ base: '100vw', md: 'min(960px, 90svw)' }}
     >
-      <Box className={{ base: '', md: 'hero-text-parallax-effect' }} mx='auto'>
-        <Flex
-          direction='column'
-          textAlign='center'
-          spacing={4}
-          w='100%'
-          bgColor='rgba(255, 255, 255, 0.4)'
-          px={{ base: 4, sm: 8, md: 16 }}
-          py={4}
-          borderRadius={{ base: '0', md: '16' }}
+      <Flex
+        direction='column'
+        textAlign='center'
+        spacing={4}
+        w='100%'
+        px={{ base: 4, sm: 8, md: 16 }}
+        py={4}
+        pb={16}
+        borderRadius={{ base: '0', md: '16' }}
+      >
+        <Text
+          as='h1'
+          className='yeseva-one-regular'
+          fontWeight='100'
+          lineHeight='relaxed'
+          color='gray.800'
+          mb='12px'
+          fontSize={{ base: 'sm', sm: 'sm', md: 'md' }}
         >
-          <Heading
-            as='h1'
-            size='2xl'
-            fontWeight='bold'
-            lineHeight='tight'
-            color='gray.800'
-            mb={{ base: 4, md: 6 }}
-            fontSize={{ base: '2xl', sm: '2xl', md: '3xl' }}
-          >
-            {t('HeroTitle')}
-          </Heading>
-          <Text
-            className='yeseva-one-regular'
-            color='gray.700'
-            fontSize={{ base: '0.8rem', sm: 'md' }}
-            maxW={{ base: '300px', sm: '450px', md: '660px', lg: '800x' }}
-            lineHeight='relaxed'
-            mx='auto'
-            mb={{ base: 4, md: 6 }}
-            letterSpacing='1.1px'
-          >
-            {t('HeroText')}
-          </Text>
-          <Flex direction={{ base: 'column', md: 'row' }} gap={{ base: 2, md: 8 }} justifyContent='center'>
-            <MyLink to='/destinations'>
-              <Button size='lg' colorScheme='blackAlpha' px={16} w={{ base: '100%', md: '240px' }} isDisabled={false}>
-                {t('DestinationsButton')}
-              </Button>
-            </MyLink>
-            <MyLink to='/coaching'>
-              <Button
-                size='lg'
-                w={{ base: '100%', md: '240px' }}
-                px={16}
-                variant='solid'
-                _hover={{ bgColor: 'white', color: '#000000d0' }}
-              >
-                {t('CoachingButton')}
-              </Button>
-            </MyLink>
-          </Flex>
-        </Flex>
-      </Box>
+          {t('HeroTitle')}
+        </Text>
+        <MyLink to='/coaching'>
+            <Button
+              size='md'
+              px={8}
+              variant='transparent'
+              bgColor={'whiteAlpha.400'}
+              _hover={{ bgColor: 'whiteAlpha.600', color: '#000000d0' }}
+              borderRadius='full'
+              border={'1px solid #FFFFFF'}
+            >
+              {t('CoachingButton')}
+            </Button>
+          </MyLink>
+      </Flex>
     </Box>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import './Home.css';
-import { Box, Flex, Image, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Button, Flex, Image, Text, useBreakpointValue } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 import { Lumiflex } from 'uvcanvas';
 import HeroText from '../../components/HeroText/HeroText';
 import MyLink from '../../components/MyLink/MyLink';
+import Destinations from '../Destinations/Destinations';
 
 const Home = () => {
   const heroImageUrlSmall = '/assets/Logo_Acuarelas_redondo_248.png';
@@ -17,68 +20,151 @@ const Home = () => {
     xl: heroImageUrlXLarge,
   });
 
+  const { t } = useTranslation('Home');
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const parallax = document.querySelector('.parallax-image');
+      if (parallax) {
+        parallax.style.transform = `translateY(${scrollY * 0.2}px)`;
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+
   return (
     <>
       <Box
-        minHeight='100lvh'
         className='hero-background'
+        overflow='hidden'
         w='100%'
-        h='100%'
+        h='40lvh'
         position='absolute'
         top='0px'
         left='0px'
-        opacity='0.9'
+        opacity='0.8'
+        borderRadius='0% 0% 40% 60% / 0% 0% 41% 47% '
       >
         <Lumiflex />
       </Box>
+
       <Flex
         className='hero'
         direction='column'
         justifyContent={{ base: 'space-between', md: 'center' }}
         w='100%'
-        minH='100lvh'
-        maxH='1400px'
-        mt='-72px'
-        gap={{ base: 0, md: 'clamp(2rem, 4.5vh, 24rem)' }}
+        h="42lvh"
+        mt='-132px'
+        mb='0px'
+        p='80px'
+        gap={{ base: 1, md: 'clamp(2rem, 4.5vh, 24rem)' }}
         mx='auto'
         alignItems='center'
         pb={{ base: 0, md: 4 }}
       >
         <Flex
-          flex={1}
           className='hero-image-wrapper'
           direction='column'
           alignItems='center'
           justifyContent='center'
-          w='100%'
-          h='100%'
+          w='50%'
         >
           <Flex
             className='hero-image-container'
             direction='column'
             alignItems='center'
             justifyItems='center'
-            w='clamp(300px, 60vmin, 1024px)'
-            p={4}
+            w='clamp(150px, 30vmin, 1024px)'
+            p={2}
           >
-            <MyLink to='/destinations'>
-              <Image
-                bgColor='#FFFFFF'
-                id='Amate_Viajando_Hero_img'
-                src={heroImageUrl}
-                alt='Amate_Viajando_Logo'
-                objectFit='cover'
-                zIndex='1'
-                opacity='0.8'
-                mt={{ base: 0, md: 8 }}
-                border='10px solid #FFFFFF'
-                borderRadius='100%'
-              />
-            </MyLink>
+            <Image
+              bgColor='#FFFFFF'
+              id='Amate_Viajando_Hero_img'
+              src={heroImageUrl}
+              alt='Amate_Viajando_Logo'
+              objectFit='cover'
+              zIndex='1'
+              opacity='0.8'
+              mt={{ base: 0, md: 8 }}
+              border='2px solid #FFFFFF'
+              borderRadius='100%'
+            />
           </Flex>
         </Flex>
         <HeroText />
       </Flex>
+
+      <Flex pt='10lvh' my='4' direction='column' alignItems='center' justifyContent='center'>
+        <Text
+          className='yeseva-one-regular'
+          color='gray.700'
+          fontSize={{ base: 'sm', sm: 'md' }}
+          maxW={{ base: '300px', sm: '450px', md: '660px', lg: '800x' }}
+          lineHeight='relaxed'
+          textAlign='center'
+          mx='auto'
+          mb={{ base: 4, md: 6 }}
+          letterSpacing='1.1px'
+        >
+          {t('HeroSubtitle')}
+        </Text>
+
+        <MyLink to='https://wa.me/5492942639282?text=Hola%20Amate%20Viajando,%20me%20gustaría%20saber%20más%20sobre%20el%20coaching%20emocional.'>
+          <Button
+            size='md'
+            w={{ base: '100%', md: '240px' }}
+            px={8}
+            mb='12'
+            variant='solid'
+            _hover={{ bgColor: 'gray.200', color: '#000000d0' }}
+          >
+            {t('PersonalAdviceButton')}
+          </Button>
+        </MyLink>
+      </Flex>
+
+      <Box
+        className='HomeImageContainer'
+        w='100%'
+        h={{ base: '60px', sm: '100px', md: '120px', lg: '160px' }}
+        overflow='hidden'
+        position='relative'
+      >
+        <Box
+          position='absolute'
+          bottom='0'
+          left='0'
+          w='100%'
+          h='25lvh'
+          bgImage="url('/beach.jpg')"
+          bgSize='cover'
+          bgPosition='center'
+          transform='translateY(0px)'
+          opacity={0.75}
+          transition='transform 0.05s ease-out'
+          className='parallax-image'
+        />
+      </Box>
+
+      <Text
+        className='yeseva-one-regular'
+        color='gray.700'
+        fontSize={{ base: 'sm', sm: 'md' }}
+        maxW={{ base: '300px', sm: '450px', md: '660px', lg: '800x' }}
+        lineHeight='relaxed'
+        textAlign='start'
+        my='6'
+        mx='4'
+        letterSpacing='1.1px'
+      >
+        {t('DestinationsButton')}
+      </Text>
+      <Destinations />
     </>
   );
 };
